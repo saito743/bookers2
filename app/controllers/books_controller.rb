71d9f8ed6bfaa2ca9e97580881a1b08ab18e4d71
saito743 <yuka.saito21@gmail.com>
2020-09-16 01:45:45 +0000

    [Add] createのelseã

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "successfully created book!"
     else
+      @books = Book.all
       render 'index'
     end
   end
